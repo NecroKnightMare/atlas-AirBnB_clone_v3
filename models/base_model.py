43bf96659844a086b0df4b0a41d76387b388c432
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-13 17:53:52 +0000

    added storage to import models

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from uuid import uuid4
 from sqlalchemy import Column, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
-from models import storage_type
+from models import storage
 import models
 
 Base = declarative_base()
