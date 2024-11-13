a4d57c8296ee91581f762722f4f0860a0cc7e4ad
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-13 20:35:38 +0000

    forgot comma

--- models/place.py
+++ models/place.py
@@ -12,7 +12,7 @@ import models
 
 
 place_amenity = Table(
-        "place_amenity", Base.metadata
+        "place_amenity", Base.metadata,
         Column(
             "place_id",
             String(60),
