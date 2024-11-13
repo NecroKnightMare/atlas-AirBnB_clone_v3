51dcf906c9f86483f56e28533d8dee364501a8fd
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-13 04:06:21 +0000

    indented line 16 again

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,7 +13,7 @@ from models.user import User
 
 # Here's a dictionary of classes for future use
 classes = {"amenities": Amenity, "cities": City,
-    "places": Place, "reviews": Review, "states": State, "users": User}
+        "places": Place, "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
