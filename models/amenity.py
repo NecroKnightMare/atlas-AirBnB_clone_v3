d029c8755413a43111c9a303cf02e881f263a2a0
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-14 13:26:12 +0000

    erased import

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,6 @@ Amenity class that inherits from BaseModel, Base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 class Amenity(BaseModel, Base):
     """
