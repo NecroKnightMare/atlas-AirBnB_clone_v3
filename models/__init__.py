fedc23f8dd239c82ac3d387ebf0bbfb768aae952
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-14 13:00:31 +0000

    removed unneccessary storage declares

--- models/__init__.py
+++ models/__init__.py
@@ -6,10 +6,8 @@ storage_type = getenv('HBNB_TYPE_STORAGE, file')
 
 if storage_type == 'db':
     from models.engine.db_storage import DBStorage as Storage
-    storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage as Storage
-    storage = FileStorage()
 
 # storage = Storage()
 storage.reload()
