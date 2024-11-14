d981e0247433b03ed8a8473f1fba301b05a1490f
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-14 12:49:53 +0000

    reverted back to older code

--- models/__init__.py
+++ models/__init__.py
@@ -2,14 +2,15 @@
 
 from os import getenv
 
-storage_type = getenv('HBNB_TYPE_STORAGE')
+storage_type = getenv('HBNB_TYPE_STORAGE, file')
+db = getenv('HBNB_ENV, 'dev')
 
 if storage_type == 'db':
-    from models.engine.db_storage import DBStorage
+    from models.engine.db_storage import DBStorage aas Storage
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
+    from models.engine.file_storage import FileStorage as Storage
     storage = FileStorage()
 
-# storage = Storage()
+storage = Storage()
 storage.reload()
